06d8fb99744e86304bfebb3a7e477f4deb923c76
miwowa1337 <miwow@inboxa.de>
2026-05-22 14:53:21 -0500

    Create thememanager.lua

---
+++ thememanager.lua
@@ -0,0 +1,168 @@
+-- again shit code
+
+
+local cloneref = (cloneref or clonereference or function(instance: any) return instance end)
+local httpService = cloneref(game:GetService('HttpService'))
+local isfolder, isfile, listfiles = isfolder, isfile, listfiles;
+local assert = function(condition, errorMessage) 
+    if (not condition) then
+        error(if errorMessage then errorMessage else "assert failed", 3)
+    end
+end
+
+if typeof(copyfunction) == "function" then
+    local isfolder_copy, isfile_copy, listfiles_copy = copyfunction(isfolder), copyfunction(isfile), copyfunction(listfiles);
+    local isfolder_success, isfolder_error = pcall(function()
+        return isfolder_copy("test" .. tostring(math.random(1000000, 9999999)))
+    end);
+
+    if isfolder_success == false or typeof(isfolder_error) ~= "boolean" then
+        isfolder = function(folder)
+            local success, data = pcall(isfolder_copy, folder)
+            return (if success then data else false)
+        end;
+        isfile = function(file)
+            local success, data = pcall(isfile_copy, file)
+            return (if success then data else false)
+        end;
+        listfiles = function(folder)
+            local success, data = pcall(listfiles_copy, folder)
+            return (if success then data else {})
+        end;
+    end
+end
+
+local ThemeManager = {} do
+	ThemeManager.Folder = 'swaghack'
+	ThemeManager.Library = nil
+	ThemeManager.BuiltInThemes = {
+		['Default'] = { 1, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"0b0b0b","AccentColor":"4a6582","BackgroundColor":"080808","OutlineColor":"141414","Glowcolor":"4a6582"}') },
+	  ['Lucid.bid'] = { 2, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"0d111a","AccentColor":"eab308","BackgroundColor":"090d13","OutlineColor":"1a2232","Glowcolor":"fbbf24"}') },
+		['Fatality'] = { 3, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"221b46","AccentColor":"d74a7a","BackgroundColor":"181233","OutlineColor":"342e57","Glowcolor":"d74a7a"}') },
+		['Night'] = { 4, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"0e0918","AccentColor":"9d6df7","BackgroundColor":"08060e","OutlineColor":"1f1433","Glowcolor":"9d6df7"}') },
+		['Quartz'] = { 5, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1b1d23","AccentColor":"5ac8fa","BackgroundColor":"121317","OutlineColor":"2b2e34","Glowcolor":"5ac8fa"}') },
+		['OneTap'] = { 6, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"22232b","AccentColor":"e2b768","BackgroundColor":"191a1f","OutlineColor":"32333a","Glowcolor":"e2b768"}') },
+	}
+
+	function ThemeManager:SetLibrary(library) self.Library = library
+    	if library and not getgenv().Library 
+		then
+        getgenv().Library = library end
+	end
+
+	function ThemeManager:GetPaths()
+	    local paths = {}
+		local parts = self.Folder:split('/')
+		for idx = 1, #parts do
+			paths[#paths + 1] = table.concat(parts, '/', 1, idx)
+		end
+		paths[#paths + 1] = self.Folder .. '/themes'
+		return paths
+	end
+
+	function ThemeManager:BuildFolderTree()
+		local paths = self:GetPaths()
+		for i = 1, #paths do
+			local str = paths[i]
+			if isfolder(str) then continue end
+			makefolder(str)
+		end
+	end
+
+	function ThemeManager:ApplyTheme(theme)
+		local data = self.BuiltInThemes[theme]
+		if not data then return end
+
+		local scheme = data[2]
+		for idx, col in next, scheme do
+			self.Library[idx] = Color3.fromHex(col)
+			if self.Library.Options[idx] then
+				self.Library.Options[idx]:SetValueRGB(Color3.fromHex(col))
+			end
+		end
+		self:ThemeUpdate()
+	end
+
+	function ThemeManager:ThemeUpdate()
+		local options = { "FontColor", "MainColor", "AccentColor", "BackgroundColor", "OutlineColor", "Glowcolor" }
+		for i, field in next, options do
+			if self.Library.Options and self.Library.Options[field] then
+				self.Library[field] = self.Library.Options[field].Value
+			end
+		end
+		self.Library.AccentColorDark = self.Library:GetDarkerColor(self.Library.AccentColor);
+		self.Library:UpdateColorsUsingRegistry()
+	end
+
+	function ThemeManager:LoadDefault()
+		local theme = 'Default'
+		local content = isfile(self.Folder .. '/themes/default.txt') and readfile(self.Folder .. '/themes/default.txt')
+		if content and self.BuiltInThemes[content] then
+			theme = content
+		elseif self.BuiltInThemes[self.DefaultTheme] then
+			theme = self.DefaultTheme
+		end
+		self.Library.Options.ThemeManager_ThemeList:SetValue(theme)
+	end
+
+	function ThemeManager:SaveDefault(theme)
+		writefile(self.Folder .. '/themes/default.txt', theme)
+	end
+
+	function ThemeManager:CreateThemeManager(groupbox)
+		groupbox:AddLabel('Background Color'):AddColorPicker('BackgroundColor', { Default = self.Library.BackgroundColor });
+		groupbox:AddLabel('Main Color'):AddColorPicker('MainColor', { Default = self.Library.MainColor });
+		groupbox:AddLabel('Accent Color'):AddColorPicker('AccentColor', { Default = self.Library.AccentColor });
+		groupbox:AddLabel('Outline Color'):AddColorPicker('OutlineColor', { Default = self.Library.OutlineColor });
+		groupbox:AddLabel('Font Color'):AddColorPicker('FontColor', { Default = self.Library.FontColor }); -- changed color in all this to capital
+		groupbox:AddLabel('Glow Color'):AddColorPicker('Glowcolor', { Default = self.Library.Glowcolor });
+		
+		local ThemesArray = {}
+		for Name, Theme in next, self.BuiltInThemes do
+			table.insert(ThemesArray, Name)
+		end
+		table.sort(ThemesArray, function(a, b) return self.BuiltInThemes[a][1] < self.BuiltInThemes[b][1] end)
+
+		--groupbox:AddDivider() no divider
+		groupbox:AddDropdown('ThemeManager_ThemeList', { Text = 'Theme List', Values = ThemesArray, Default = 1 })
+		groupbox:AddButton('Set as default', function()
+			self:SaveDefault(self.Library.Options.ThemeManager_ThemeList.Value)
+			self.Library:Notify(string.format('set default theme to %q', self.Library.Options.ThemeManager_ThemeList.Value)) -- make notifys lowercase
+		end)
+
+		self.Library.Options.ThemeManager_ThemeList:OnChanged(function()
+			self:ApplyTheme(self.Library.Options.ThemeManager_ThemeList.Value)
+		end)
+
+		self:LoadDefault()
+
+		local function UpdateTheme() self:ThemeUpdate() end
+		self.Library.Options.BackgroundColor:OnChanged(UpdateTheme)
+		self.Library.Options.MainColor:OnChanged(UpdateTheme)
+		self.Library.Options.AccentColor:OnChanged(UpdateTheme)
+		self.Library.Options.OutlineColor:OnChanged(UpdateTheme)
+		self.Library.Options.FontColor:OnChanged(UpdateTheme)
+		self.Library.Options.Glowcolor:OnChanged(UpdateTheme)
+	end
+
+	function ThemeManager:CreateGroupBox(tab)
+		assert(self.Library, 'ThemeManager:CreateGroupBox -> Must set ThemeManager.Library first!')
+		return tab:AddLeftGroupbox('Themes')
+	end
+
+	function ThemeManager:ApplyToTab(tab)
+		assert(self.Library, 'ThemeManager:ApplyToTab -> Must set ThemeManager.Library first!')
+		local groupbox = self:CreateGroupBox(tab)
+		self:CreateThemeManager(groupbox)
+	end
+
+	function ThemeManager:ApplyToGroupbox(groupbox)
+		assert(self.Library, 'ThemeManager:ApplyToGroupbox -> Must set ThemeManager.Library first!')
+		self:CreateThemeManager(groupbox)
+	end
+
+	ThemeManager:BuildFolderTree()
+end
+
+getgenv().LinoriaThemeManager = ThemeManager
+return ThemeManager
